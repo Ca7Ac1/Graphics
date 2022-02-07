f02aa875687ad172b28f1a1fa6e2c50a74c78d2f
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-02-06 20:36:14 -0500

    completed work

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+#include "window.hpp"
+
+using namespace std;
+
+int main()
+{
+    string file = "pic.ppm";
+    Window win(file, 600, 600);
+
+    for (int i = 0; i < win.getX(); i++)
+    {
+        for (int j = 0; j < win.getY(); j++)
+        {
+            if (i * j <= 9000)
+            {
+                win[i][j].set(255, 0, 0);
+            }
+            else if (i * j <= 35000)
+            {
+                win[i][j].set(200, 55, 0);
+            }
+            else if (i * j <= 72000)
+            {
+                win[i][j].set(100, 155, 0);
+            }
+            else if (i * j <= 110000)
+            {
+                win[i][j].set(100, 155, 100);
+            }
+            else if (i * j <= 150000)
+            {
+                win[i][j].set(0, 255, 00);
+            }
+            else if (i * j <= 180000)
+            {
+                win[i][j].set(0, 200, 55);
+            }
+            else if (i * j <= 220000)
+            {
+                win[i][j].set(0, 100, 155);
+            }
+            else if (i * j <= 360000)
+            {
+                win[i][j].set(0, 0, 255);
+            }
+        }
+    }
+
+    win.display();
+
+    return 0;
+}
