55c42bb9f047b427092f9add86bf539614fd5e6a
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-02-27 00:58:51 -0500

    test matrix mult

--- main.cpp
+++ main.cpp
@@ -4,6 +4,38 @@
 #include "renderer.hpp"
 #include "graphics.hpp"
 
+using namespace std;
+
+void testMatrix()
+{
+    Matrix m1;
+    Graphics m2;
+    cout << "\nTesting add_edge. Adding (1, 2, 3), (4, 5, 6) m2 =";
+    m2.addEdge(1, 2, 3, 4, 5, 6);
+    m2.printEdges();
+
+    cout << "Testing ident. m1 = ";
+    m1.setIdentity();
+    m1.print();
+
+    cout << "\nTesting matrix_mult. m1 * m2 =";
+    m2.transform(m1);
+    m2.printEdges();
+
+    m1.clear();
+    m1.add(Point(1, 2, 3));
+    m1.add(Point(4, 5, 6));
+    m1.add(Point(7, 8, 9));
+    m1.add(Point(10, 11, 12));
+
+    cout << "\nTesting Matrix mult. m1 =";
+    m1.print();
+
+    cout << "\nTesting Matrix mult. m1 * m2 =";
+    m2.transform(m1);
+    m2.printEdges();
+}
+
 int main()
 {
     Window win("main.ppm", 500, 500, true);
