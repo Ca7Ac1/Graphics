d0fa09a9a91faa42965214e55db16de2c74b660d
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-03-23 01:00:12 -0400

    makefile and main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "window.hpp"
+#include "renderer.hpp"
+#include "script.hpp"
+
+using namespace std;
+
+int main()
+{
+    Window win(500, 500);
+    Renderer r(win);
+
+    win.invertY();
+
+    parse(win, r, "script");
+}
