c6b779b37770ba4767c41cba490efd1b6571b771
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-05-08 17:04:14 -0400

    make main show all scripts

--- main.cpp
+++ main.cpp
@@ -13,8 +13,34 @@ int main()
 
     win.invertY();
 
-    r.setColor(50, 255, 255);
+    r.setColor(1, 255, 255);
     r.addPointLight(.5, .75, 1);
 
     parse(win, r, "script");
+
+    r.clearZBuffer();
+    r.clearLights();
+    r.clearPlane();
+
+    r.setColor(0, 0, 0);
+    r.fill();
+
+    r.setColor(255, 1, 255);
+    r.addPointLight(.5, .75, 1);
+
+    r.deletePlane();
+
+    parse(win, r, "script_robot");
+
+    r.clearZBuffer();
+    r.clearLights();
+    r.clearPlane();
+
+    r.setColor(0, 0, 0);
+    r.fill();
+
+    r.setColor(255, 255, 1);
+    r.addPointLight(.5, .75, 1);
+
+    parse(win, r, "script_shapes");
 }
