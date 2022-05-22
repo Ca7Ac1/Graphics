2ed0045a46d3dc3b572b0a163888f0749c4f44e8
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-05-22 14:57:57 -0400

    tab formatting

--- mdl/symtab.hpp
+++ mdl/symtab.hpp
@@ -10,7 +10,7 @@
 #define SYM_VALUE 2
 #define SYM_CONSTANTS 3
 #define SYM_LIGHT 4
-#define SYM_FILE   5
+#define SYM_FILE 5
 #define SYM_STRING 5
 
 #define AMBIENT_R 0
@@ -19,28 +19,29 @@
 
 struct constants
 {
-  double r[4];
-  double g[4];
-  double b[4];
-  double red,green,blue;
+	double r[4];
+	double g[4];
+	double b[4];
+	double red, green, blue;
 };
 
 struct light
 {
-  double l[4];
-  double c[4];
+	double l[4];
+	double c[4];
 };
 
 typedef struct
 {
-  char *name;
-  int type;
-  union{
-    Matrix *m;
-    struct constants *c;
-    struct light *l;
-    double value;
-  } s;
+	char *name;
+	int type;
+	union
+	{
+		Matrix *m;
+		struct constants *c;
+		struct light *l;
+		double value;
+	} s;
 } SYMTAB;
 
 extern "C" SYMTAB symtab[MAX_SYMBOLS];
@@ -52,6 +53,5 @@ void print_constants(struct constants *p);
 void print_light(struct light *p);
 void print_symtab();
 void set_value(SYMTAB *p, double value);
-
 
 #endif
